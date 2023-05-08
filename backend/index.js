8c0b7cbb7748b2a51d9a6d87b8337f5bed6dc3c6
Ashish-A-Kulkarni <ashishkulkarni410@gmail.com>
2023-05-08 16:17:03 +0530

    display dummy chat

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const {chats} = require("./data/data")
+const dotenv = require("dotenv");
+const res = require("express/lib/response");
+
+
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
-app.listen(5000 , console.log("server started on PORT 5000"));
+app.get("/",(req,res) => {
+    res.send("Home Page CHAT APP");
+});
+
+app.get("/api/chat", (req,res) => {
+    res.send(chats);
+});
+
+app.get("/api/chat/:id", (req,res) => {
+    const reqchat = chats.find((c) => c._id === req.params.id);
+    res.send(reqchat);
+});
+
+app.listen(5000 , console.log(`Server started on PORT ${PORT}`));
